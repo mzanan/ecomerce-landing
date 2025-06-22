c9861cb1c84e41494fca992f4413a69347efc7c3
Matias Zanan <matiaszanan@gmail.com>
2025-06-22 19:09:46 +0700

    Update dependencies & remove shinnyButton

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    dirs: ['src'],
+  },
 };
 
 export default nextConfig;
